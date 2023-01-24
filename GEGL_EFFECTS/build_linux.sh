87121071db1a712992cf89bf7d06b315296d6749
Daniele Scasciafratte <mte90net@gmail.com>
2023-01-24 17:41:05 +0100

    feat(linux): improved scripts

--- GEGL_EFFECTS/build_linux.sh
+++ GEGL_EFFECTS/build_linux.sh
@@ -2,3 +2,15 @@
 
 
 meson setup --buildtype=release build && ninja -C build
+
+cd ./compile_innerglow_here
+./build_linux.sh
+
+cd ../compile_bevel_here
+./build_linux.sh
+
+cd ../
+
+mv ./build/gegleffects.so ~/.local/share/gegl-0.4/plug-ins/gegleffects.so
+mv ./compile_innerglow_here/build/inner-glow.so ~/.local/share/gegl-0.4/plug-ins/inner-glow.so
+mv ./compile_bevel_here/build/bevel.so ~/.local/share/gegl-0.4/plug-ins/bevel.so
