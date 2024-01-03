0d14a5e672f91efea8b91eff8f233783de4bb1ec
LinuxBeaver <78667207+LinuxBeaver@users.noreply.github.com>
2024-01-03 13:30:11 -0500

    Update build_everything_windows_mysys2_only.sh
    Windows and Linux "click to build all".sh files updated.

--- build_everything_windows_mysys2_only.sh
+++ build_everything_windows_mysys2_only.sh
@@ -1,3 +1,14 @@
 #!/bin/bash
 
- cd compile_bevel_here/ && ./build_linux.sh && cd .. && cd compile_innerglow_here/ && ./build_linux.sh && cd .. && cd compile_glass_over_text_here/ && ./build_linux.sh && cd .. && cd compile_shiny_surface_here/ && ./build_linux.sh && cd .. && cd compile_zzstrokebevelimage_here/ && ./build_linux.sh && cd .. && cd compile_gegl_effects_here/ && ./build_linux.sh && cd .. && mv compile_bevel_here/build/bevel.dll bevel.dll && mv compile_innerglow_here/build/inner-glow.dll inner-glow.dll && mv compile_glass_over_text_here/build/glassovertext.dll glassovertext.dll && mv compile_shiny_surface_here/build/shinytext.dll shinytext.dll && mv compile_zzstrokebevelimage_here/build/zzstrokebevelimage.dll zzstrokebevelimage.dll && mv compile_gegl_effects_here/build/gegleffectspending.dll gegleffectspending.dll && rm -r compile_bevel_here/build && rm -r compile_gegl_effects_here/build && rm -r compile_glass_over_text_here/build && rm -r compile_innerglow_here/build && rm -r compile_shiny_surface_here/build && rm -r compile_zzstrokebevelimage_here/build 
+for dir in *
+do
+    if test -f "$dir/build_linux.sh"
+    then
+      echo "building in $dir"
+     ( cd "$dir" && bash ./build_linux.sh ) || exit 1
+    fi
+done
+
+  mkdir -p WindowsBinaries
+  mv $(find . -name '*.dll') WindowsBinaries/
+
