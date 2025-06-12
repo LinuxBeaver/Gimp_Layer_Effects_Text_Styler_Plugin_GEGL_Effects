e08fd90551c4220a8908e2ecaf4a60bbe359c83f
LinuxBeaver <78667207+LinuxBeaver@users.noreply.github.com>
2025-06-12 18:18:34 -0400

    Update effects.c

--- SourceCode/gegl_effects/effects.c
+++ SourceCode/gegl_effects/effects.c
@@ -13,7 +13,7 @@
  * You should have received a copy of the GNU Lesser General Public
  * License along with GEGL; if not, see <https://www.gnu.org/licenses/>.
  *
- * Copyright 2006 Øyvind Kolås <pippin@gimp.org>
+ * Credit to Øyvind Kolås (pippin) for major GEGL contributions
  *2022-2025 Beaver GEGL Effects  
  *2022 BarefootLiam (for helping give Inner Glow a disable checkbox)
 
