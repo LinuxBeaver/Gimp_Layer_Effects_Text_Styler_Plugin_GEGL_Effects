0ccadcaceafd35a5d92c743b3414b505ea392f06
LinuxBeaver <78667207+LinuxBeaver@users.noreply.github.com>
2025-08-13 11:11:10 -0400

    Add files via upload

--- SourceCode/ssg/ssg.c
+++ SourceCode/ssg/ssg.c
@@ -13,7 +13,7 @@
  * You should have received a copy of the GNU Lesser General Public
  * License along with GEGL; if not, see <https://www.gnu.org/licenses/>.
  *
- * Copyright 2006 Øyvind Kolås <pippin@gimp.org>
+ * Credit to Øyvind Kolås (pippin) for major GEGL contributions
  * 2023 Beaver (GEGL SSG (stroke, shadow, glow) )
  */
 
