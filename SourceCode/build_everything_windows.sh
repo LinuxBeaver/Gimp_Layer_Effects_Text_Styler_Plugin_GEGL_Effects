071afef32b7598d722e8a0f497612efdf2950ec0
LinuxBeaver <78667207+LinuxBeaver@users.noreply.github.com>
2025-09-25 21:37:40 -0400

    Add files via upload

---
+++ SourceCode/build_everything_windows.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+mkdir WindowsBinaries
+
+for dir in *
+do
+    if test -f "$dir/build_linux.sh"
+    then
+      echo "building in $dir"
+     ( cd "$dir" && bash ./build_linux.sh ) || exit 1
+    fi
+done
+
+
+  mv $(find . -name '*.dll') WindowsBinaries/
+
+
